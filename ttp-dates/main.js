0ef05213fabc0885ba15233fb4fd03509d366bb1
William Yau <yauyinx@gmail.com>
2021-08-25 20:52:33 -0400

    Update main.js

--- ttp-dates/main.js
+++ ttp-dates/main.js
@@ -8,10 +8,28 @@ const url = 'https://ttp.cbp.dhs.gov/schedulerapi/slots?orderBy=soonest&limit=20
   const dateTimes = appointmentTimes.map(slot => mapToDateTime(slot.startTimestamp));
   console.log(dateTimes);
   
-  new Notification('testing');
+  notify('testing');
 })();
 
 function mapToDateTime(stringDateTime) {
   const [input, year, month, date, hour, minute] = stringDateTime.match(/^(\d+)-(\d+)-(\d+)T(\d+):(\d+)$/);
   return {year, month, date, hour, minute};
+}
+
+async function notify(message) {
+  if (!('Notification' in window)) {
+    console.error("This browser does not support desktop notification");
+    return;
+  }
+  
+  if (Notification.permission !== 'denied') {
+    await Notification.requestPermission();
+  }
+  
+    
+  if (permission !== 'granted') {
+    console.warn('User denied notification permission request');
+  } else {
+    new Notification(message);
+  }
 }
